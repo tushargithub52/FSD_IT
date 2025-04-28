d7262238cb56ca533c06751c822b0010fee5b141
tushargithub52 <tusharraiku6020@gmail.com>
2025-04-28 12:47:11 +0530

    cors edit

--- PART 2 - BACKEND/USERAPP/Server/app.js
+++ PART 2 - BACKEND/USERAPP/Server/app.js
@@ -1,11 +1,16 @@
 const express=require("express");
+const cors = require("cors")
 const app=express();
 const dotenv=require("dotenv");
 const userroute=require("./Routes/userRoutes")
 const connectDB=require("./config/db")
+
 dotenv.config();
+
 const Port=process.env.PORT || 3000;
 connectDB();
+
+app.use(cors())
 app.use(express.json());
 app.use("/",userroute)
 app.listen(Port,()=>{
